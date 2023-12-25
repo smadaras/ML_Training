1796ac354f612c3127858851ed768af4a58658c8
smadaras <76752573+smadaras@users.noreply.github.com>
2023-12-26 00:46:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ import altair as alt
 import streamlit as st
 
 movies_2000 = pd.read_csv('cost_revenue_clean.csv')
-# print(movies_2000)
+st.write(movies_2000)
+exit()
 st.altair_chart(alt.Chart(movies_2000))
