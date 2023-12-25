235dd2518fc90808389a59a85d1e990cc972adb9
smadaras <76752573+smadaras@users.noreply.github.com>
2023-12-26 00:31:21 +0100

    chart fix

--- app.py
+++ app.py
@@ -7,8 +7,14 @@ movies_2000 = pd.read_csv('cost_revenue_clean.csv')
 # print(movies_2000)
 
 
-st.altair_chart(movies_2000).mark_circle(size=100).encode(
+st.altair_chart(alt.Chart((movies_2000).mark_circle(size=100).encode(
     x=alt.X('production_budget_usd', title = "Budget in USD"),
     y=alt.Y('worldwide_gross_usd', title = "Gross in USD"),
-    tooltip=['teaching', 'citations']).interactive()
+    tooltip=['teaching', 'citations']))
+
+"""
+st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+        .mark_circle(color='#0068c9', opacity=0.5)
+        .encode(x='x:Q', y='y:Q'))
+"""
     
