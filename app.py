1ee0f25944c7f1d3efb843d4b222178ed45f6ba2
smadaras <76752573+smadaras@users.noreply.github.com>
2023-12-26 00:34:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,3 @@ st.altair_chart(alt.Chart((movies_2000).mark_circle(size=100).encode(
     x=alt.X('production_budget_usd', title = "Budget in USD"),
     y=alt.Y('worldwide_gross_usd', title = "Gross in USD"),
     tooltip=['teaching', 'citations']))
-
-
-#st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-#        .mark_circle(color='#0068c9', opacity=0.5)
-#        .encode(x='x:Q', y='y:Q'))
