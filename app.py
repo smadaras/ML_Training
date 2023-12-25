a1dbe90f8cce8c3d57eedaa0be9ac843bc9d177e
smadaras <76752573+smadaras@users.noreply.github.com>
2023-12-26 00:37:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ movies_2000 = pd.read_csv('cost_revenue_clean.csv')
 # print(movies_2000)
 
 
-st.altair_chart(alt.Chart((movies_2000).mark_circle(size=100).encode(
-    x=alt.X('production_budget_usd', title = "Budget in USD"),
-    y=alt.Y('worldwide_gross_usd', title = "Gross in USD"),
-    tooltip=['teaching', 'citations']))
+st.altair_chart(
+    alt.Chart(
+        (movies_2000).mark_circle(size=100).encode(
+            x=alt.X('production_budget_usd', title = "Budget in USD"),
+            y=alt.Y('worldwide_gross_usd', title = "Gross in USD"),
+            tooltip=['teaching', 'citations'])
+    )
+)
