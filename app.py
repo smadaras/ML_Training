5f68bac045c0d2e89c7d4b6a0174f91e64668922
smadaras <76752573+smadaras@users.noreply.github.com>
2023-12-26 16:08:23 +0100

    regression 01

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+# import numpy as np
+from sklearn.linear_model import LinearRegression
 import altair as alt
 
 
 movies_2000 = pd.read_csv('cost_revenue_clean.csv')
 st.write(movies_2000)
-# exit()
-c = (
-   alt
-      .Chart(movies_2000)
-      .mark_point()
-      .encode(
-         alt.X('production_budget_usd'),
-         alt.Y('worldwide_gross_usd')
-      )
-)
 
 st.altair_chart(
    alt.Chart(movies_2000)
@@ -25,3 +16,7 @@ st.altair_chart(
          alt.Y('worldwide_gross_usd')
       )
 )
+
+regression = LinearRegression()
+regression.fit(X, y)
+print(f"y = {regression.coef_}*x + {regression.intercept_}")
