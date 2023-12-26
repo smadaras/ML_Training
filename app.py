aea2f70d391e7477446322d05fe6e156a51fb599
smadaras <76752573+smadaras@users.noreply.github.com>
2023-12-26 22:47:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,24 +11,14 @@ st.write(data)
 X = pd.DataFrame(data, columns=['production_budget_usd'])
 y = pd.DataFrame(data, columns=['worldwide_gross_usd'])
 
-
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-
-"""c = (
-   alt.Chart(chart_data)
-   .mark_circle()
-   .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
-)
-st.altair_chart(c, use_container_width=True)"""
-
 d = (
    alt.Chart(data)
    .mark_point(filled = True)
    .encode(
       #alt.X('production_budget_usd'),
       #alt.Y('worldwide_gross_usd')
-      x = X,
-      y = Y
+      X,
+      y
       )
 )
 st.altair_chart(d, use_container_width=True)
