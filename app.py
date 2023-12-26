3838060f4915d8017e37a41f02a934f50ba2354e
smadaras <76752573+smadaras@users.noreply.github.com>
2023-12-26 16:14:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 # from sklearn.linear_model import LinearRegression
-import sklearn.linear_model
+import sklearn
 import altair as alt
 
 
