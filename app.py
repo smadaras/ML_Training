a6c93c3d37d59a2bc936f04523a29716fc609405
smadaras <76752573+smadaras@users.noreply.github.com>
2023-12-26 16:35:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import altair as alt
 
 movies_2000 = pd.read_csv('cost_revenue_clean.csv')
 st.write(movies_2000)
+X = DataFrame(data, columns=['production_budget_usd'])
+y = DataFrame(data, columns=['worldwide_gross_usd'])
 
 st.altair_chart(
    alt.Chart(movies_2000)
