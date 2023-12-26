c603aa90453b191a47c54d15f005bbbe1e6a4682
smadaras <76752573+smadaras@users.noreply.github.com>
2023-12-26 22:29:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,14 +11,25 @@ st.write(data)
 X = pd.DataFrame(data, columns=['production_budget_usd'])
 y = pd.DataFrame(data, columns=['worldwide_gross_usd'])
 
-st.altair_chart(
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+c = (
+   alt.Chart(chart_data)
+   .mark_circle()
+   .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+st.altair_chart(c, use_container_width=True)
+
+d = (
    alt.Chart(data)
-      .mark_point(filled = True)
-      .encode(
-         alt.X('production_budget_usd'),
-         alt.Y('worldwide_gross_usd')
+   .mark_point(filled = True)
+   .encode(
+      alt.X('production_budget_usd'),
+      alt.Y('worldwide_gross_usd')
       )
 )
+st.altair_chart(d, use_container_width=True)
 
 regression = LinearRegression()
 regression.fit(X, y)
