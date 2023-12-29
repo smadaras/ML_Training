1910b046b275a2bdbc5d1d8d39e5ec713ead84b9
smadaras <76752573+smadaras@users.noreply.github.com>
2023-12-29 13:37:37 +0100

    print data only

--- pages/2_Tissues.py
+++ pages/2_Tissues.py
@@ -6,7 +6,7 @@ import altair as alt
 
 
 data = pd.read_csv('lsd_math_score_data.csv')
-st.write(data.description())
+st.write(data)
 """X = pd.DataFrame(data, columns=['production_budget_usd'])
 y = pd.DataFrame(data, columns=['worldwide_gross_usd'])
 
