c01c5267f7e7b4677f2e1735b1077e0231a371f2
smadaras <76752573+smadaras@users.noreply.github.com>
2024-01-04 00:10:18 +0100

    first commit

---
+++ pages/3_Altair_Training.py
@@ -0,0 +1,17 @@
+import altair as alt
+import streamlit as st
+
+# load a simple dataset as a pandas DataFrame
+from vega_datasets import data
+cars = data.cars()
+
+chartA = alt.Chart(cars).mark_point().encode(
+    x='Horsepower',
+    y='Miles_per_Gallon',
+    color='Origin',
+).interactive()
+
+st.altair_chart(
+    chartA,
+    use_container_width = True
+)
